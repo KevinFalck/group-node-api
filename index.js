af6b65e794e88f4d9ef1fd5023aa9c1360f47a6c
KevinFalck <kevin.falck@gmail.com>
2025-12-15 13:26:12 +0000

    feat: add POST /users endpoint to create new users.

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const port = 3000;
 
 app.use(express.json());
 
+let users = [
+    { id: 1, nom: "Alice" },
+    { id: 2, nom: "Bob" }
+    { id: 3, nom: "Charlie" }
+];
+
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    users.push(newUser);
+    res.json(newUser);
+});
+
 app.listen(port, () => {
     console.log(`Serveur en écoute sur http://localhost:${port}`);
 });
